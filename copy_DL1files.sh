dd88fa8f660ba14d536776c85ba3efe378c730a1
GernotMaier <gernot.maier@desy.de>
2020-08-24 08:17:23 +0200

    add target directory EVNDISP

--- copy_DL1files.sh
+++ copy_DL1files.sh
@@ -13,23 +13,24 @@ fi
 
 # hardwired values
 DSET="Prod5_LaPalma_AdvancedBaseline_NSB1x_DL1"
-PLIST="gamma_onSource gamma_cone proton electron"
 PLIST="gamma_onSource gamma_cone proton"
+PLIST="gamma_onSource gamma_cone proton electron"
 # targetdir
-OSET="prod5-LaPalma-20deg-w01-LL"
+OSET="prod5-LaPalma-20deg-EVNDISP"
 ########################
 # list of arrays
 # South
 # declare -a ARRAY=("BL-0LSTs15MSTs50SSTs-MSTF" "BL-0LSTs15MSTs50SSTs-MSTN" "BL-4LSTs25MSTs70SSTs-MSTF" "BL-4LSTs25MSTs70SSTs-MSTN" )
 # North
 declare -a ARRAY=("BL-4LSTs05MSTs-MSTF" "BL-4LSTs09MSTs-MSTF" "BL-4LSTs15MSTs-MSTF" ) 
+declare -a ARRAY=("BL-4LSTs15MSTs-MSTN" )
 
 for P in $PLIST
 do
    for A in "${ARRAY[@]}"
    do
        # files are copied here
-       ODIR=${CTA_USER_DATA_DIR}/analysis/AnalysisData/${OSET}/N.${A}/$P
+       ODIR=${CTA_USER_DATA_DIR}/analysis/AnalysisData/${OSET}/N.${A}/EVNDISP/$P
        echo $ODIR
        mkdir -p ${ODIR}
        find ${DSET}/$P -name "*.tar.gz" -print -exec tar --overwrite -C ${ODIR} -xvzf {} --wildcards "*${A}*" \;
