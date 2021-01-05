9b487b6c3ee2b94465dd6833e406629dc51b2f67
GernotMaier <gernot.maier@desy.de>
2021-01-05 13:15:17 +0100

    Add DL1 prod3b-SCT

--- download_parallel.sh
+++ download_parallel.sh
@@ -10,12 +10,17 @@ DD="Prod5_Paranal_AdvancedBaseline_NSB1x"
 DD="Prod4b_Paranal"
 # Prod4b SST production
 DD="Prod3b_Paranal"
+# Prod5 NSB 1x
+DD="Prod5b_LaPalma_AdvancedBaseline_NSB1x"
+# Prod3B SCT DL1
+DD="Prod3b_Paranal_20deg_HB9_SCT_DL1"
 
 ZE="20deg"
 DL="DL0"
 SCT="FALSE"
-if [[ $DD == "Prod3b_Paranal" ]]; then
+if [[ $DD == *"Prod3b_Paranal"* ]]; then
   DL="HB9"
+  DL="HB9_SCT_DL1"
   SCT="TRUE"
 fi
 
@@ -44,6 +49,15 @@ do
                 FFLIST="${DD}_${ZE}/${DD}_${PP}_${A}_${ZE}_SSTOnly_${S}_${DL}.GRID.list"
                 ./getRawFilesFromGRID-DIRAC.sh ${FFLIST} ${DD}/${P}/ 100 &> tmp_log/${P}_${A}_${S}.log &
             done
+         # Prod3b SCT DL1
+         elif [[ $DD = *"Prod3b"* ]] && [[ $DD = *"SCT_DL1"* ]]; then
+             FLIST="${DD}/Prod3_Paranal_${PP}_${A}_${ZE}_${DL}.GRID.list"
+             if [[ ! -e ${FLIST} ]]; then
+               echo "$FLIST not found"
+               exit
+            fi
+            echo "...found ${FLIST}"
+            ./getRawFilesFromGRID-DIRAC.sh ${FLIST} ${DD}/${P}/ 100 &> tmp_log/${P}_${A}_${S}.log
          # all other productions
          else
             FLIST="${DD}/${DD}_${PP}_${A}_${ZE}_${DL}.GRID.list"
@@ -58,7 +72,7 @@ do
                 echo "...found ${FLIST}"
             fi
             echo "Reading file list $FLIST"
-            ./getRawFilesFromGRID-DIRAC.sh $FLIST ${DD}/${P}/ 100 ${SCT} &> tmp_log/${P}_${A}.log &
+            #./getRawFilesFromGRID-DIRAC.sh $FLIST ${DD}/${P}/ 100 ${SCT} &> tmp_log/${P}_${A}.log &
          fi
          sleep 10
      done
